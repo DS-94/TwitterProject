53e1375cbefae8d677462bdfa080e50fd580743d
GMiller95 <gmills95@hotmail.co.uk>
2016-09-15 15:50:59 +0100

    Weather Dictionary

---
+++ WeatherDict.py
@@ -0,0 +1,13 @@
+weatherdict = {'NA': ['Not available', 'sunny', 'raining', 'windy'], 0: ['Clear night', 'cold', 'windy', 'stars']\
+, 1: ['Sunny day', 'sunny', 'hot', 'boiling'], 2: ['Partly cloudy (night)', 'cloudy', 'gloomy', 'overcast' ], 3: ['Partly cloudy (day)', 'cloudy', 'gloomy', 'overcast'],\
+ 4: 'Not used', 5:['Mist', 'mist', 'misty', 'foggy'], 6: ['Fog', 'fog', 'foggy', 'misty'], 7:['Cloudy', 'cloudy', 'overcast', 'raining'],\
+8:['Overcast', 'overcast', 'cloudy', 'raining'], 9:['Light rain shower (night)', 'rain', 'raining', 'drizzle'], 10:['Light rain shower (day)', 'rain', 'raining', 'drizzle']\
+ 11: ['Drizzle', 'drizzle', 'rain', 'rain'], 12:['Light rain', 'rain', 'raining', 'drizzle'], 13:['Heavy rain shower (night)', 'rain', 'raining', 'pouring']\
+,14:['Heavy rain shower (day)', 'raining', 'pouring', 'rain'], 15:['Heavy rain', 'rain', 'raining', 'pouring'], 16:['Sleet shower (night)', 'sleet', 'raining', 'snow'] \
+, 17: ['Sleet shower (day)', 'sleet', 'raining', 'snow'], 18:['Sleet', 'sleet', 'raining', 'snow']\
+, 19:['Hail shower (night)', 'hail', 'stormy', 'raining'], 20:['Hail shower (day)', 'hail', 'raining', 'stormy']\
+, 21: ['Hail', 'hail', 'raining', 'stormy'], 22:['Light snow shower (night)', 'snow', 'sleet', 'raining']\
+, 23:['Light snow shower (day)', 'snow', 'sleet', 'raining'], 24: ['Light snow', 'snow', 'snowing', 'raining']\
+, 25:['Heavy snow shower (night)', 'snow', 'snowing', ''], 26:['Heavy snow shower (day)', 'snow', 'snowing', ''],\
+27:['Heavy snow', 'snow', 'snowing', ''], 28:['Thunder shower (night)', 'thunder', 'lightning', 'raining'],\
+29:['Thunder shower (day)', 'thunder', 'lightning', 'raining'], 30:['Thunder', 'thunder', 'lightning', 'raining']}
